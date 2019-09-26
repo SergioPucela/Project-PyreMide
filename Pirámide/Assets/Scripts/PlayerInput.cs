d6bb3914766229303c24b0d1bd7e468aa6f5a331
SergioPucela <44496290+SergioPucela@users.noreply.github.com>
2019-09-26 16:33:42 +0200

    First player input added

--- Pirámide/Assets/Scripts/PlayerInput.cs
+++ Pirámide/Assets/Scripts/PlayerInput.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    bool modoVista = false;
+    public Rotacion jugador;
+    public Rotacion camara;
 
     // Update is called once per frame
     void Update()
     {
-        
+        GetInput();
+    }
+
+    void GetInput()
+    {
+        if (Input.GetKeyDown("v"))
+        {
+            if (modoVista)
+            {
+                camara.enabled = true;
+                jugador.enabled = false;
+                modoVista = !modoVista;
+            }
+            else
+            {
+                jugador.enabled = true;
+                camara.enabled = false;
+                modoVista = !modoVista;
+            }
+        }
     }
 }
